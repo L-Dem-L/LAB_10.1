2c8cfbe757c131bb18714ad3aa8a4d23776853b1
Demian Rymar <ua.demyan@gmail.com>
2024-04-04 22:04:39 +0300

    Add project files.

---
+++ UnitTest/UnitTest.cpp
@@ -0,0 +1,40 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../LAB_10.1/Main.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest
+{
+	TEST_CLASS(UnitTest)
+	{
+	public:
+		
+		TEST_METHOD(TestContainsThreeConsecutiveStars)
+		{
+            // Arrange
+            const char* testFilename = "test.txt";
+
+            // Act
+            ofstream testFile(testFilename);
+            testFile << "No stars here" << endl;
+            testFile << "*** Three stars ***" << endl;
+            testFile << "One * star" << endl;
+            testFile << "Two ** stars" << endl;
+            testFile << "No consecutive stars" << endl;
+            testFile << "***** Consecutive stars *****" << endl;
+            testFile.close();
+
+            // Assert
+            Assert::IsTrue(ContainsThreeConsecutiveStars(testFilename));
+
+            // Arrange
+            testFile.open(testFilename, ofstream::out | ofstream::trunc);
+            testFile << "No consecutive stars" << endl;
+            testFile.close();
+
+            // Assert
+            Assert::IsFalse(ContainsThreeConsecutiveStars(testFilename));
+		}
+	};
+}
