efb36d4394490cb4595c7945aabca8287aa0b874
Demian Rymar <ua.demyan@gmail.com>
2024-04-05 00:23:07 +0300

    Fixed

--- LAB_10.1/Main.cpp
+++ LAB_10.1/Main.cpp
@@ -3,29 +3,29 @@
 #include <string>
 
 bool hasTripleStars(const std::string& filename) {
-    std::ifstream file(filename); // Open file for reading
+    std::ifstream file(filename);
     if (!file.is_open()) {
         std::cerr << "Failed to open the file " << filename << std::endl;
         return false;
     }
 
     std::string line;
-    while (std::getline(file, line)) { // Read file line by line
-        size_t found = line.find("***"); // Search for a group of three asterisks
+    while (std::getline(file, line)) { 
+        size_t found = line.find("***");
         if (found != std::string::npos) {
-            file.close(); // Close the file
-            return true; // Return true if the group is found
+            file.close();
+            return true;
         }
     }
 
-    file.close(); // Close the file
-    return false; // Return false if the group is not found
+    file.close();
+    return false;
 }
 
 int main() {
     std::string filename;
     std::cout << "Enter the filename to search for triple stars: ";
-    std::cin >> filename; // Input the filename
+    std::cin >> filename;
     if (hasTripleStars(filename)) {
         std::cout << "The file contains a group of three consecutive stars." << std::endl;
     }
